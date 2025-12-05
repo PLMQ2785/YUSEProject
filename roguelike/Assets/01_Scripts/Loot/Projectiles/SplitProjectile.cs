525f1555a36e86f50f3a60b5aa0240cd60cc759b
DenyAir <kjkkjs5252@gmail.com>
2025-12-05 22:20:09 +0900

    풀 리퀘스트 병합 #146
    * feat : 장비 구현
    
    * Merge branch 'PLMQ2785:develop' into refactor/mainmenu

---
+++ roguelike/Assets/01_Scripts/Loot/Projectiles/SplitProjectile.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+
+public class SplitProjectile : Projectile
+{
+
+    [Header("Split Setting")]
+
+    [SerializeField] private float _rotationSpeed = 360f;
+    [SerializeField] private bool _clockTurn = true;
+    private Vector3 _moveDirection;
+
+
+    public void initialize(float damage, float speed)
+    {
+        _damage = damage;
+        _speed = speed; 
+    }
+
+
+    protected override void Start()
+    {
+        base.Start();
+        _moveDirection = transform.right;
+
+        if (Random.value > 0.5f)
+        {
+            _clockTurn = true;
+        }
+        else
+        {
+            _clockTurn = false;
+        }
+    }
+
+
+
+    protected override void UpdateMovement()
+    {
+        transform.position += _moveDirection * _speed * Time.deltaTime;
+
+        float rotAmount= _rotationSpeed * Time.deltaTime;
+        if (_clockTurn)
+            rotAmount *= -1;
+
+        transform.Rotate(0,0,rotAmount);
+    }
+
+}
